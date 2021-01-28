bb61621aad99f95e34eebb4067277cfd00d8232f
WWS321 <849750445@qq.com>
2021-01-28 18:22:33 +0800

    Create iboxpayCOOKIE.js

---
+++ Task/iboxpayCOOKIE.js
@@ -0,0 +1,15 @@
+
+
+//独立COOKIE文件     ck在``里面填写，多账号换行 304000000000000000000161174345799300002bfa26fc
+let iboxpayheaderVal = ``
+
+
+
+
+
+let iboxpaycookie = {
+    iboxpayheaderVal: iboxpayheaderVal,
+
+}
+
+module.exports = iboxpaycookie
